08d7276f4715d2949cd9a652be98ef13d755328b
BangMango <103486068+BangMango@users.noreply.github.com>
2022-04-12 17:06:28 +0900

    Update 최소대금.java

--- eclipse-workspace/Algorithm/src/codeUp/최소대금.java
+++ eclipse-workspace/Algorithm/src/codeUp/최소대금.java
@@ -3,7 +3,7 @@ package codeUp;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class 최소대금 {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
